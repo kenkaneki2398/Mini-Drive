1df84a58afbd2eed73e0a3b0b4dce4f77420b619
kenkaneki <lunafreya2398@gmail.com>
2021-01-28 22:47:06 +0700

    Routing setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,28 @@
-import Signup from "./Signup";
-import { Container } from 'react-bootstrap'
-import { AuthProvider } from '../contexts/AuthContext'
+import React from "react"
+import Signup from "./Signup"
+import { Container } from "react-bootstrap"
+import { AuthProvider } from "../contexts/AuthContext"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Dashboard from './Dashboard'
 
 function App() {
   return (
-    <AuthProvider>
-      <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
-        <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Signup />
-        </div>
-      </Container>
-    </AuthProvider>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route path="/signup" component={Signup} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
   )
 }
 
-export default App;
+export default App
