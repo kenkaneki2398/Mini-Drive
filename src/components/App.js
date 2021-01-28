37dae4c946fccb475778a22fecc02ac468e70465
kenkaneki <lunafreya2398@gmail.com>
2021-01-28 19:55:42 +0700

    Signup component functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import Signup from "./Signup";
 import { Container } from 'react-bootstrap'
+import { AuthProvider } from '../contexts/AuthContext'
 
 function App() {
   return (
-    <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
-      <div className="w-100" style={{ maxWidth: "400px" }}>
-        <Signup />
-      </div>
-    </Container>
+    <AuthProvider>
+      <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Signup />
+        </div>
+      </Container>
+    </AuthProvider>
   )
 }
 
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,17 +1,40 @@
-import React, { useRef } from 'react'
-import { Form, Button, Card } from 'react-bootstrap'
+import React, { useRef, useState } from 'react'
+import { Form, Button, Card, Alert } from 'react-bootstrap'
+import { useAuth } from '../contexts/AuthContext'
 
 export default function Signup() {
-    const emailRef = useRef();
-    const passwordRef = useRef();
-    const passwordConfirmRef = useRef();
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const passwordConfirmRef = useRef()
+    const { signup } = useAuth()
+    const [error, setError] = useState('')
+    const [loading, setLoading] = useState(false)
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+
+        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+            return setError('Mật khẩu không trùng khớp !')
+        }
+
+        try {
+            setError('')
+            setLoading(true)
+            await signup(emailRef.current.value, passwordRef.current.value)
+        } catch {
+            setError('Đăng ký tài khoản không thành công !')
+        }
+
+        setLoading(false)
+    }
 
     return (
         <>
             <Card>
                 <Card.Body>
                     <h2 className="text-center mb-4">ĐĂNG KÝ</h2>
-                    <Form>
+                    {error && <Alert variant="danger">{error}</Alert>}
+                    <Form onSubmit={handleSubmit}>
                         <Form.Group id="email">
                             <Form.Label>Email</Form.Label>
                             <Form.Control type="email" ref={emailRef} required></Form.Control>
@@ -24,7 +47,7 @@ export default function Signup() {
                             <Form.Label>Nhập lại mật khẩu</Form.Label>
                             <Form.Control type="password" ref={passwordConfirmRef} required></Form.Control>
                         </Form.Group>
-                        <Button className="w-100" type="submit">Đăng ký</Button>
+                        <Button disabled={loading} className="w-100" type="submit">Đăng ký</Button>
                     </Form>
                 </Card.Body>
             </Card>
